243020a7ed036edc3a15920f78dfe57e26e5eceb
Jame <cike534222598@qq.com>
2016-06-29 13:41:19 +0800

    Initial Commit

---
+++ WeChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeChat
+//
+//  Created by Microseer on 16/6/29.
+//  Copyright © 2016年 Jame. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeChat
+//
+//  Created by Microseer on 16/6/29.
+//  Copyright © 2016年 Jame. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
